88e8c95c61558e697a73a5bf14839fad2dd88baf
mindstorm38 <theorozier38@gmail.com>
2018-06-13 20:00:57 +0200

    Making 1 class for all Collections or arrays like class

--- src/main/java/io/sutil/CollectionUtils.java
+++ src/main/java/io/sutil/CollectionUtils.java
@@ -2,10 +2,12 @@ package io.sutil;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-public class ListUtils {
+public class CollectionUtils {
 	
 	@SafeVarargs
 	public static <E> List<E> createListInline(E...elts) {
@@ -41,6 +43,29 @@ public class ListUtils {
 	
 	public static <E> void listDiff(List<E> from, List<E> to) {
 		listDiff( from, to, new EqualTester.EqualTesterDefault<>() );
+	}
+
+	public static <K, V> V getOrDefaultKey(Map<K, V> map, K key, K defkey) {
+		V o = map.get( key );
+		return o == null ? map.get( defkey ) : o;
+	}
+
+	public static <K, V> V getOrDefault(Map<K, V> map, K key, V defValue) {
+		V o = map.get( key );
+		return o == null ? defValue : o;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <K, V> Map<K, V> createMapInline(Object...kva) {
+		Map<K, V> map = new HashMap<>();
+		try {
+			for ( int i = 0; i < kva.length; i++ ) {
+				map.put( (K) kva[ i ], (V) kva[ i + 1 ] );
+			}
+		} catch (ClassCastException e) {
+			throw new IllegalStateException( "Illegal key or value class", e );
+		}
+		return map;
 	}
 	
 }
