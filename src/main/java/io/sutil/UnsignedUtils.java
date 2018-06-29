dca5fccebb6dd0c952a941a45e62733cc4485df8
mindstorm38 <theorozier38@gmail.com>
2018-06-29 23:51:32 +0200

    Changed unsigned types max values

--- src/main/java/io/sutil/UnsignedUtils.java
+++ src/main/java/io/sutil/UnsignedUtils.java
@@ -2,9 +2,9 @@ package io.sutil;
 
 public class UnsignedUtils {
 
-	public static final int UNSIGNED_BYTE_MAX_VALUE		= 0xFF;
-	public static final int UNSIGNED_SHORT_MAX_VALUE	= 0xFFFF;
-	public static final int UNSIGNED_INTEGER_MAX_VALUE	= 0xFFFFFFFF;
+	public static final short UNSIGNED_BYTE_MAX_VALUE		= (short) ( Math.pow( 2, 8 ) - 1 );
+	public static final int UNSIGNED_SHORT_MAX_VALUE		= (int) ( Math.pow( 2, 16 ) - 1 );
+	public static final long UNSIGNED_INTEGER_MAX_VALUE		= (long) ( Math.pow( 2, 32 ) - 1 );
 	// Reminder : Unsigned long have no sense
 	
 }
