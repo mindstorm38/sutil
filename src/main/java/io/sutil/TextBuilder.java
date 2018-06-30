b95761974d00a4c2cd82514bf4f4e4cab7d8db5e
mindstorm38 <theorozier38@gmail.com>
2018-06-30 23:50:05 +0200

    Adding TextBuilder

---
+++ src/main/java/io/sutil/TextBuilder.java
@@ -0,0 +1,35 @@
+package io.sutil;
+
+public class TextBuilder {
+
+	private char[] data;
+	
+	public TextBuilder() {
+		
+		this.data = new char[0];
+		
+	}
+	
+	public TextBuilder append(Object obj) {
+		
+		char[] chars = ( obj instanceof Character ) ? new char[] { (Character) obj } : obj.toString().toCharArray();
+		
+		char[] newData = new char[ data.length + chars.length ];
+		System.arraycopy( this.data, 0, newData, 0, this.data.length );
+		System.arraycopy( chars, 0, newData, this.data.length, chars.length );
+		this.data = newData;
+		
+		return this;
+		
+	}
+	
+	public TextBuilder nl() {
+		return this.append('\n');
+	}
+	
+	@Override
+	public String toString() {
+		return new String( this.data );
+	}
+	
+}
