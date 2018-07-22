06adfeca83b4d28807dbb4c51a162df83573b88b
mindstorm38 <theorozier38@gmail.com>
2018-07-22 23:53:15 +0200

    Added a default parent logger

--- src/main/java/io/sutil/LoggerUtils.java
+++ src/main/java/io/sutil/LoggerUtils.java
@@ -1,5 +1,8 @@
 package io.sutil;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -7,32 +10,45 @@ import java.util.logging.Logger;
 
 public class LoggerUtils {
 	
+	private static final DateFormat DATE_FORMAT;
+	
+	public static final Logger DEFAULT_PARENT_LOGGER;
 	public static final Logger LOGGER;
 	
 	static {
 		
-		LOGGER = Logger.getLogger("SUtil");
-		LOGGER.setUseParentHandlers( false );
-		LOGGER.setLevel( Level.ALL );
-		LOGGER.setParent( Logger.getLogger("SUtil Default Parent Logger") );
+		DATE_FORMAT = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss.SSS");
 		
-		LOGGER.addHandler( new Handler() {
+		DEFAULT_PARENT_LOGGER = Logger.getLogger("SUtil Default Logger");
+		DEFAULT_PARENT_LOGGER.setUseParentHandlers( false );
+		DEFAULT_PARENT_LOGGER.addHandler( new Handler() {
 			
+			@Override
 			public void publish(LogRecord record) {
-				Logger logger = LOGGER.getParent();
-				if ( logger == null ) return;
-				record.setMessage( "[" + record.getLoggerName() + "] " + record.getMessage() );
+				
+				String stacktrace = record.getThrown() == null ? "" : "\n" + StringUtils.getStackTraceString( record.getThrown() );
+				
+				System.out.println( "[" + DATE_FORMAT.format( new Date() ) + "] [" + record.getLoggerName() + "] [" + record.getLevel().getName() + "] " + ( record.getMessage() == null ? "" : String.format( record.getMessage(), record.getParameters() ) + " " ) + ( record.getThrown() == null ? "" : record.getThrown().getClass().getName() + " : " + record.getThrown().getMessage() + stacktrace ) );
+				
 			}
 			
+			@Override
 			public void flush() {}
+			
+			@Override
 			public void close() throws SecurityException {}
 			
 		} );
 		
+		LOGGER = Logger.getLogger("SUtil");
+		LOGGER.setUseParentHandlers( true );
+		LOGGER.setLevel( Level.ALL );
+		LOGGER.setParent( DEFAULT_PARENT_LOGGER );
+		
 	}
 	
 	public static void setParentLogger(Logger logger) {
-		LOGGER.setParent( logger );
+		LOGGER.setParent( logger == null ? DEFAULT_PARENT_LOGGER : logger );
 	}
 	
 }
