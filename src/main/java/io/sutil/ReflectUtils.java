46980f55bf676e8396141f73d4299ad0a1c7f0e1
mindstorm38 <theorozier38@gmail.com>
2018-07-23 18:39:25 +0200

    Added comments in ReflectUtils

--- src/main/java/io/sutil/ReflectUtils.java
+++ src/main/java/io/sutil/ReflectUtils.java
@@ -1,16 +1,34 @@
 package io.sutil;
 
+/**
+ * 
+ * Utilities for the Java Runtime Reflection
+ * 
+ * @author Mindstorm38
+ *
+ */
 public class ReflectUtils {
 	
+	/**
+	 * Initialize classes, it also call the static initializer of the class
+	 * @param classes Classes to initialize
+	 */
 	public static void initClasses(Class<?>...classes) {
 		for ( Class<?> clazz : classes ) {
 			try { Class.forName( clazz.getName() ); } catch (ClassNotFoundException e) {}
 		}
 	}
 	
-	public static <E> E safecast(Object buffer, Class<E> clazz) {
-		if ( !clazz.isAssignableFrom( buffer.getClass() ) ) return null;
-		return clazz.cast( buffer );
+	/**
+	 * Safe cast an object or return <code>null</code> if unassignable
+	 * @param object The object to cast
+	 * @param clazz The cast class
+	 * @param <E> The type of the <code>clazz</code> and of returned object
+	 * @return Casted object
+	 */
+	public static <E> E safecast(Object object, Class<E> clazz) {
+		if ( !clazz.isAssignableFrom( object.getClass() ) ) return null;
+		return clazz.cast( object );
 	}
 	
 }
