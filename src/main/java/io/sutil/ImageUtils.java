73200cae15b2ecf097a56238435b96dfd853b3e0
mindstorm38 <theorozier38@gmail.com>
2018-07-29 10:16:53 +0200

    Added readBufferedImageSafe

---
+++ src/main/java/io/sutil/ImageUtils.java
@@ -0,0 +1,29 @@
+package io.sutil;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class ImageUtils {
+	
+	/**
+	 * Read a {@link BufferedImage} from an {@link InputStream} without throwing error like {@link ImageIO#read(InputStream)}<br>
+	 * This function close the stream at the end of reading
+	 * @param stream Image input stream
+	 * @return 
+	 */
+	public static BufferedImage readBufferedImageSafe(InputStream stream) {
+		
+		try {
+			return ImageIO.read( stream );
+		} catch (IOException e) {
+			return null;
+		} finally {
+			StreamUtils.safeclose( stream );
+		}
+		
+	}
+	
+}
