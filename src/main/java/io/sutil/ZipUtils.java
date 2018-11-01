d8a3aa716943e4263b77f2567c4cbc378a9ed60d
mindstorm38 <theorozier38@gmail.com>
2018-11-01 11:39:25 +0100

    Added null check for zip extract method and added doc

--- src/main/java/io/sutil/ZipUtils.java
+++ src/main/java/io/sutil/ZipUtils.java
@@ -1,5 +1,6 @@
 package io.sutil;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,12 +8,26 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+/**
+ * 
+ * @author Mindstorm38
+ *
+ */
 public class ZipUtils {
 	
-	public static void extractZipToDirectory(File file, File dir, Consumer<Float> progress) throws IOException {
+	/**
+	 * Extract the content of a ZIP archive to a directory
+	 * @param file The file ZIP archive
+	 * @param dir The directory where to extract 
+	 * @param filter An optional filter for select entries
+	 * @param progress An optional float consumer for progress
+	 * @throws IOException For all stream operations
+	 */
+	public static void extractZipToDirectory(File file, File dir, Predicate<ZipEntry> filter, Consumer<Float> progress) throws IOException {
 		
 		try ( ZipFile zip = new ZipFile( file ) ) {
 			
@@ -28,6 +43,10 @@ public class ZipUtils {
 			while ( entries.hasMoreElements() ) {
 				
 				entry = entries.nextElement();
+				
+				if ( filter != null && !filter.test( entry ) )
+					continue;
+				
 				extractfile = new File( dir, entry.getName() );
 				
 				if ( entry.isDirectory() ) {
@@ -38,7 +57,7 @@ public class ZipUtils {
 					
 					stream = zip.getInputStream( entry );
 					
-					try ( OutputStream output = new FileOutputStream( extractfile ) ) {
+					try ( OutputStream output = new BufferedOutputStream( new FileOutputStream (extractfile ) ) ) {
 						StreamUtils.copyStream( stream, output );
 					}
 					
@@ -48,7 +67,8 @@ public class ZipUtils {
 				
 				i++;
 				
-				progress.accept( (float) i / size );
+				if ( progress != null )
+					progress.accept( (float) i / size );
 				
 			}
 			
