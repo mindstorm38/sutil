acc35b9887b961d697988f23037f2860b6b36d06
Théo Rozier <contact@theorozier.fr>
2020-05-25 08:16:43 +0200

    Little optimization on UnsignedUtils.

--- src/main/java/io/sutil/UnsignedUtils.java
+++ src/main/java/io/sutil/UnsignedUtils.java
@@ -2,9 +2,9 @@ package io.sutil;
 
 public class UnsignedUtils {
 
-	public static final short UNSIGNED_BYTE_MAX_VALUE		= (short) ( Math.pow( 2, 8 ) - 1 );
-	public static final int UNSIGNED_SHORT_MAX_VALUE		= (int) ( Math.pow( 2, 16 ) - 1 );
-	public static final long UNSIGNED_INTEGER_MAX_VALUE		= (long) ( Math.pow( 2, 32 ) - 1 );
+	public static final short UNSIGNED_BYTE_MAX_VALUE		= (short) (1 << 8) - 1;
+	public static final int UNSIGNED_SHORT_MAX_VALUE		= (int) (1 << 16) - 1;
+	public static final long UNSIGNED_INTEGER_MAX_VALUE		= (long) (1 << 32) - 1;
 	// Reminder : Unsigned long have no sense
 	
 }
